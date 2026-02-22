8e6dc60a9a8ec5932823149e1551827e7784b611
성대권 <seong@seongdaegwon-ui-Macmini.local>
2026-02-23 08:57:41 +0900

    [Fix] Rails 시간대를 Seoul로 설정 - Date.today가 UTC 기준으로 동작하여 하루 전 날짜 표시되는 문제 해결

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Silmu
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Seoul"
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
